4230e1b7dab285e87b36af62b0c91729dcaf79c6
Queke <nicolas.granados@virginiogomez.cl>
2021-05-17 15:52:46 -0400

    se siguen añadiendo cosas básicas

--- Ejercicio 3.js
+++ Ejercicio 3.js
@@ -12,16 +12,60 @@ let objeto = { // objetos
     edad: 22
 }
 console.log("Mi nombre es: ", objeto.nombre, "Y mi edad es:", objeto.edad)
+
+
 paso()
+console.log("Ciclo While")
 paso()
 
-switch (status){
-    case 0:
-        break;
-    case 1:
-        break;
-    default:
-        break;
+// ciclo whie
+
+let contador = 0
+let N = 50
+while (contador <= N){
+    console.log(contador)
+    contador++
 }
+
+
+// el for es el de toda la vida
+paso()
+console.log("Ciclo FOR")
+paso()
+for(let i=0;i<10;i++){
+    console.log(i)
+}
+
+// for of y for in
+
+paso()
+console.log("FOR of y FOR in")
+paso()
+
+let array2 = [10,20,30]
+let estudiantes = [
+    {
+        nombre: 'Estudiante 1',
+        nota: 5
+    },
+    {
+        nombre: 'Estudiante 2',
+        nota: 6
+    },
+    {
+        nombre: 'Estudiante 3',
+        nota: 1
+    }
+]
+// forma normal
+for(let i=0;i<estudiantes.length;i++){
+   console.log(estudiantes[i].nombre, estudiantes[i].nota)
+}
+paso()
+//esta echo para vectores 
+for (let estudiante of estudiantes) {
+    console.log(estudiante.nombre, estudiante.nota)
+}
+
 
 
