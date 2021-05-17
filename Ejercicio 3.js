8ebfa510a53316b5d6e6b53d7bbf3e59b380207a
Queke <nicolas.granados@virginiogomez.cl>
2021-05-17 12:33:01 -0400

    se hacen nuevos ejercicios sobre array y otros

---
+++ Ejercicio 3.js
@@ -0,0 +1,27 @@
+//Array
+function paso (){
+    console.log("----------")
+}
+
+let array = [0,1,2,3,4,5,6] // arreglo o vector
+console.log(array)
+
+let objeto = { // objetos 
+    nombre: 'Nicolas',
+    apellido: 'Granados',
+    edad: 22
+}
+console.log("Mi nombre es: ", objeto.nombre, "Y mi edad es:", objeto.edad)
+paso()
+paso()
+
+switch (status){
+    case 0:
+        break;
+    case 1:
+        break;
+    default:
+        break;
+}
+
+
